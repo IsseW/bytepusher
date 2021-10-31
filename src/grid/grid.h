ea26d636e5bc307e073a0dcee3ca03939e9003d1
Isse <isidornie@gmail.com>
2021-10-31 14:30:23 +0100

    render grid

--- src/grid/grid.h
+++ src/grid/grid.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <memory>
 enum class Tile : uint8_t {
   Dead = 0x0,
@@ -9,8 +10,8 @@ struct Grid {
  private:
   uint32_t width;
   uint32_t height;
-  std::unique_ptr<Tile[]> current_data;
-  std::unique_ptr<Tile[]> working_data;
+  std::unique_ptr<uint8_t[]> current_data;
+  std::unique_ptr<uint8_t[]> working_data;
 
   void swap();
 
@@ -33,4 +34,6 @@ struct Grid {
   Grid(uint32_t width, uint32_t height);
 
   void update_with_rule(void (*rule)(Grid&, uint32_t x, uint32_t y));
+
+  const uint8_t* get_texture();
 };
--- src/grid/rules.h
+++ src/grid/rules.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "grid.h"
 
 void game_of_life_rule(Grid& grid, uint32_t x, uint32_t y);
